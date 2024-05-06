dd8a31d97c48dbd54e9a0ca6c96fabc0051a383e
senzj <soulcrepzyt@gmail.com>
2024-05-07 00:23:12 +0800

    button changes

--- application/views/page/services/ser_select_edit.php
+++ application/views/page/services/ser_select_edit.php
@@ -7,32 +7,28 @@
                     
 				<div style="width: 100%; padding: 15px; border-style: solid; border-color:#404040; border-width: medium; background-color: #F2F2F2; color: #000000; margin-bottom:5%; border-radius:24px; text-align: left;">
 
-						<a href="<?php echo $this->config->base_url("/index.php/ser_edit")?>">
-							<button type="button" class="btn btn-primary" style="width: 100%; text-align: center; padding: 2%; border-style: solid; border-color:#404040; border-width: medium; background-color: #0097B2; font-size: 24px; color: #000000; margin-bottom:2%; text-align: left;">
-								<p>NAME: SERVICE NAME</p>
-								<p>DESC: SERVICE DESCRIPTION</p>
-								<p>PRICE: SERVICE PRICE</p>
-							</button>
-						</a>
+						<!-- use for each loop to get everydata and display it as a list -->
+
+						<!-- To submit data just use segment like base_url("/index.php/ser_edit/segment(3)") maybe using get function-->
 
 						<a href="<?php echo $this->config->base_url("/index.php/ser_edit")?>">
-							<button type="button" class="btn btn-primary" style="width: 100%; text-align: center; padding: 2%; border-style: solid; border-color:#404040; border-width: medium; background-color: #0097B2; font-size: 24px; color: #000000; margin-bottom:2%; text-align: left;">
-								<p>NAME: SERVICE NAME</p>
-								<p>DESC: SERVICE DESCRIPTION</p>
-								<p>PRICE: SERVICE PRICE</p>
-							</button>
-						</a>
-						
-						<a href="<?php echo $this->config->base_url("/index.php/ser_edit")?>">
-							<button type="button" class="btn btn-primary" style="width: 100%; text-align: center; padding: 2%; border-style: solid; border-color:#404040; border-width: medium; background-color: #0097B2; font-size: 24px; color: #000000; margin-bottom:2%; text-align: left;">
-								<p>NAME: SERVICE NAME</p>
-								<p>DESC: SERVICE DESCRIPTION</p>
-								<p>PRICE: SERVICE PRICE</p>
+							<button class="btnpushable btnStyle cyan" style="color: black;">
+								<span class="btnshadow"></span>
+								<span class="btnedge"></span>
+								<span class="btnfront">
+									<p>NAME: SERVICE NAME</p>
+									<p>DESC: SERVICE DESCRIPTION</p>
+									<p>PRICE: SERVICE PRICE</p>
+								</span>
 							</button>
 						</a>
 
                         <a href="<?php echo $this->config->base_url("/index.php/services")?>">
-							<button style="margin-top: 5%; margin-left: 1%; width:40%; font-size:50px; background-color:#EA2D40; border-radius:15px; border-width:5px; border-color:#404040;" class="btn btn-default">RETURN</button>
+							<button class="btnpushable btnStyle red" style="margin-top: 5%; margin-left: 1%; width:40%; font-size:50px; background-color:#EA2D40; border-radius:15px; border-width:5px; border-color:#404040;">
+								<span class="btnshadow"></span>
+								<span class="btnedge"></span>
+								<span class="btnfront">BACK</span>
+							</button>
 						</a>
 
 				</div>
