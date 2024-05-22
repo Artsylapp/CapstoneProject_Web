0423158ecf517e2eb7783665e6d2fd16a243b5a7
senzj <soulcrepzyt@gmail.com>
2024-05-22 18:39:25 +0800

    API

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Capstoneproject_web/';
+// $config['base_url'] = 'http://localhost/Capstoneproject_web/';
+$config['base_url'] = 'http://192.168.254.145/Capstoneproject_web/';
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/api/ApiAuth.php
+++ application/controllers/api/ApiAuth.php
@@ -5,28 +5,48 @@ require APPPATH . 'libraries/RestController.php';
 require APPPATH . 'libraries/Format.php';
 use chriskacerguis\RestServer\RestController;
 
-class ApiAuth extends RestController{
+class ApiAuth extends RestController {
 
-    public function __construct(){
+    public function __construct() {
         parent::__construct();
+        $this->load->library('session');
         $this->load->model('Account_model');
+        $this->load->model('Company_model');
     }
 
-    public function index(){
-        $data = [
-            'company_tbl_name' => $this->post('accounts_name'),
-            'company_tbl_pass' => $this->post('accounts_pass'),
-        ];
-        $this->response($data, 200);
+    public function index_post() {
+        $comp_name = $this->post('username');
+        $comp_pass = $this->post('password');
+    
+        // Validate input
+        if (empty($comp_name) || empty($comp_pass)) {
+            $this->response([
+                'error' => true,
+                'message' => "Username and password are required"
+            ], 400);
+            return;
+        }
+    
+        // Authenticate user
+        $auth = $this->Company_model->getCompany($comp_name, $comp_pass);
+    
+        if ($auth == FALSE) {
+            $this->response([
+                'error' => true,
+                'message' => "Invalid Name or Password"
+            ], 401);
+        } else {
+            $loginData = [
+                'error' => false,
+                'message' => 'Login Successful!',
+                'user' => [
+                    'username' => $auth[0]->company_tbl_name,
+                    'password' => $auth[0]->company_tbl_pass,
+                ]
+            ];
+            $this->response($loginData, 200);
+        }
     }
 
-    public function loginauth_post(){
-        $login = [
-            'company_tbl_name' => $this->post('login_name'),
-            'company_tbl_pass' => $this->post('login_password'),
-        ];
-        $this->response($login, 200);
 
-        $data = $this->Account_model->loginAuth($login);
-    }
 }
