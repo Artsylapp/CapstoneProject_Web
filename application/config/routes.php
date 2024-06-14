17d09b18572bf4faf1e9bbb378a5470a54191581
senzj <soulcrepzyt@gmail.com>
2024-06-15 00:33:32 +0800

    commit

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,8 +52,8 @@ $route['orders_going'] = 'Orders/orders_going';
 $route['orders_cancel'] = 'Orders/orders_cancel';
 
 /* Booking page Page */
-$route['cancel'] = 'Orders/cancel_booking';
-$route['save'] = 'Orders/save_booking';
+$route['booking/cancel/(:num)'] = 'Orders/cancel_booking/$1';
+$route['booking/save'] = 'Orders/save_booking';
 
 
     /* Records Page */
