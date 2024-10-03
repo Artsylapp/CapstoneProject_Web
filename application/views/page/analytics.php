3b455b851056062a28159c913715dca30f81fab6
senzj <soulcrepzyt@gmail.com>
2024-10-03 12:25:32 +0800

    fix: UI for analytics

--- application/views/page/analytics.php
+++ application/views/page/analytics.php
@@ -1,58 +1,44 @@
 <div class="analytics">
     <!-- Dynamic layout with flexbox -->
-
-    <div class="container" style="width: 96.8vw">
-
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="charts">
-                    <div id="analysisChartContainer">
-                        <canvas id="ChartAnalysis" class="analytics_canvas"></canvas>
-                    </div>
-
-                    <div class="revenueChartContainer">
-                        <canvas id="RevenueChart"></canvas>
-                    </div>
-                </div>
-            </div>
+    <div class="charts">
+        <div id="analysisChartContainer">
+            <canvas id="ChartAnalysis"></canvas>
         </div>
 
-        <div class="row mt-s">
-            <div class="col-xs-12">
-                <div class="AnalyticsData" id="DataAnalytics" style="width:100%">
-                    <div class="title">
-                        <h2>Analytics Report</h2>
-                    </div>
-                    <div class="cardData">
-                        <p>Total Sale</p>
-                        <p><span id="totalProfit"></span></p>
-                    </div>
-                    <div class="cardData">
-                        <p>Total Number of Orders</p>
-                        <p><span id="totalOrder"></span></p>
-                    </div>
-                    <div class="cardData">
-                        <p>Most Ordered Service</p>
-                        <p><span id="mostService"></span></p>
-                    </div>
-                    <div class="cardData">
-                        <p>Overall Total Revenue</p>
-                        <p><span id="totalRevenue"></span></p>
-                    </div>
-                    <div class="cardData">
-                        <p>Average Order Value</p>
-                        <p><span id="AOV"></span></p>
-                    </div>
-                    <div class="cardData">
-                        <p>Most Active Employee</p>
-                        <p><span id="mostActiveEmployee"></span></p>
-                    </div>
-                </div>
-            </div>
+        <div class="revenueChartContainer">
+            <canvas id="RevenueChart"></canvas>
         </div>
-
     </div>
-    
+
+    <div class="AnalyticsData" id="DataAnalytics">
+        <div class="title">
+            <h2>Analytics Report</h2>
+        </div>
+        <div class="cardData">
+            <p>Total Sale</p>
+            <p><span id="totalProfit"></span></p>
+        </div>
+        <div class="cardData">
+            <p>Total Number of Orders</p>
+            <p><span id="totalOrder"></span></p>
+        </div>
+        <div class="cardData">
+            <p>Most Ordered Service</p>
+            <p><span id="mostService"></span></p>
+        </div>
+        <div class="cardData">
+            <p>Overall Total Revenue</p>
+            <p><span id="totalRevenue"></span></p>
+        </div>
+        <div class="cardData">
+            <p>Average Order Value</p>
+            <p><span id="AOV"></span></p>
+        </div>
+        <div class="cardData">
+            <p>Most Active Employee</p>
+            <p><span id="mostActiveEmployee"></span></p>
+        </div>
+    </div>
 </div>
 
 <script src="<?php echo $this->config->base_url('assets/js/ChartAnalytics.js') ?>"></script> <!-- Chart Analytics -->
