a83b1dba3e4bacdfafe26d2e1dc9597196c0ad60
senzj <soulcrepzyt@gmail.com>
2024-10-07 19:31:28 +0800

    fix: update analytics UI to Bootstrap grid layout

--- application/config/database.php
+++ application/config/database.php
@@ -77,17 +77,17 @@ $db['default'] = array(
 	'dsn'	=> '',
 
 	// if using localhost to debug
-	'hostname' => 'localhost', // Ensure MySQL is running on localhost
-	'username' => 'root', // Default MySQL username for hostinger
-	'password' => '', // No password for root user (not recommended for production)
-	'database' => 'viammdb', // Name of your database
-	'dbdriver' => 'mysqli'
+	// 'hostname' => 'localhost', // Ensure MySQL is running on localhost
+	// 'username' => 'root', // Default MySQL username for hostinger
+	// 'password' => '', // No password for root user (not recommended for production)
+	// 'database' => 'viammdb', // Name of your database
+	// 'dbdriver' => 'mysqli'
 
 	// if using hostinger
-	//'hostname' => 'localhost', // Ensure MySQL is running on localhost
-	//'username' => 'u265772247_viammDB', // Default MySQL username for hostinger
-	//'password' => 'n7JS8r88P?',
-	//'database' => 'u265772247_viammdb', // Name of your database
+	'hostname' => 'localhost', // Ensure MySQL is running on localhost
+	'username' => 'u265772247_viammDB', // Default MySQL username for hostinger
+	'password' => 'n7JS8r88P?',
+	'database' => 'u265772247_viammdb', // Name of your database
 
 	//'dbdriver' => 'mysqli', // Using MySQL Improved driver
 	//'dbprefix' => '',
--- application/views/page/analytics.php
+++ application/views/page/analytics.php
@@ -1,46 +1,78 @@
 <div class="analytics">
     <!-- Dynamic layout with flexbox -->
-    <div class="charts">
-        <div id="analysisChartContainer">
-            <canvas id="ChartAnalysis"></canvas>
-        </div>
 
-        <div class="revenueChartContainer">
-            <canvas id="RevenueChart"></canvas>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-xs-8">
+                <div class="charts" style>
+                    <div id="analysisChartContainer">
+                        <canvas id="ChartAnalysis" class="analytics_canvas"></canvas>
+                    </div>
+                    
+                    <div class="revenueChartContainer">
+                        <canvas id="RevenueChart"></canvas>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xs-4">
+                <div class="AnalyticsData" id="DataAnalytics">
+                    
+                    <div class="container">
+
+                        <div class="justify-content-center">
+                            <div class="col">
+                                <div class="title">
+                                    <h2>Analytics Report</h2>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="row">
+
+                            <div class="cardData col-md-5">
+                                <p>Total Sale</p>
+                                <p><span id="totalProfit"></span></p>
+                            </div>
+
+                            <div class="cardData col-md-6">
+                                <p>Total Number of Orders</p>
+                                <p><span id="totalOrder"></span></p>
+                            </div>
+
+                            <div class="cardData col-md-5">
+                                <p>Most Ordered Service</p>
+                                <p><span id="mostService"></span></p>
+                            </div>
+
+                            <div class="cardData col-md-6">
+                                <p>Most Active Employee</p>
+                                <p><span id="mostActiveEmployee"></span></p>
+                            </div>
+
+                            <div class="cardData col-md-5">
+                                <p>Overall Total Revenue</p>
+                                <p><span id="totalRevenue"></span></p>
+                            </div>
+
+                            <div class="cardData col-md-6">
+                                <p>Average Order Value</p>
+                                <p><span id="AOV"></span></p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+            
         </div>
     </div>
-
-    <div class="AnalyticsData" id="DataAnalytics">
-        <div class="title">
-            <h2>Analytics Report</h2>
-        </div>
-        <div class="cardData">
-            <p>Total Sale</p>
-            <p><span id="totalProfit"></span></p>
-        </div>
-        <div class="cardData">
-            <p>Total Number of Orders</p>
-            <p><span id="totalOrder"></span></p>
-        </div>
-        <div class="cardData">
-            <p>Most Ordered Service</p>
-            <p><span id="mostService"></span></p>
-        </div>
-        <div class="cardData">
-            <p>Overall Total Revenue</p>
-            <p><span id="totalRevenue"></span></p>
-        </div>
-        <div class="cardData">
-            <p>Average Order Value</p>
-            <p><span id="AOV"></span></p>
-        </div>
-        <div class="cardData">
-            <p>Most Active Employee</p>
-            <p><span id="mostActiveEmployee"></span></p>
-        </div>
-    </div>
+    
 </div>
 
 <script src="<?php echo $this->config->base_url('assets/js/ChartAnalytics.js') ?>"></script> <!-- Chart Analytics -->
 <script src="<?php echo $this->config->base_url('assets/js/DataAnalytics.js') ?>"></script> <!-- Data Analytics -->
-<script src="<?php echo $this->config->base_url('assets/js/Analytics.js') ?>"></script> <!-- Analytics -->
+<script src="<?php echo $this->config->base_url('assets/js/Analytics.js') ?>"></script> <!-- Analytics -->  
