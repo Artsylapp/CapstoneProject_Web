905fba2a66840ded463004254bc28700c26fe7d3
Artsylapp <artsylapp@gmail.com>
2024-10-09 06:00:56 +0800

    analytics minor check
    asd

--- application/views/page/analytics.php
+++ application/views/page/analytics.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
 
-            <div class="col-xs-8">
+            <div class="col-xs-9">
                 <div class="charts" style>
                     <div id="analysisChartContainer">
                         <canvas id="ChartAnalysis" class="analytics_canvas"></canvas>
@@ -16,49 +16,61 @@
                 </div>
             </div>
 
-            <div class="col-xs-4">
+            <div class="col-xs-3">
                 <div class="AnalyticsData" id="DataAnalytics">
                     
                     <div class="container">
-
-                        <div class="justify-content-center">
-                            <div class="col">
+                        <div class="row">
+                            <div class="col-xs-12 justify-content-center">
                                 <div class="title">
                                     <h2>Analytics Report</h2>
                                 </div>
                             </div>
                         </div>
                         
+                        
                         <div class="row">
 
-                            <div class="cardData col-md-5">
-                                <p>Total Sale</p>
-                                <p><span id="totalProfit"></span></p>
+                            <div class="col-xs-6">
+                                <div class="cardData">
+                                    <p>Total Sale</p>
+                                    <p><span id="totalProfit"></span></p>
+                                </div>
                             </div>
 
-                            <div class="cardData col-md-6">
-                                <p>Total Number of Orders</p>
-                                <p><span id="totalOrder"></span></p>
+                            <div class="col-xs-6">
+                                <div class="cardData">
+                                    <p>Total Number of Orders</p>
+                                    <p><span id="totalOrder"></span></p>
+                                </div>
                             </div>
 
-                            <div class="cardData col-md-5">
-                                <p>Most Ordered Service</p>
-                                <p><span id="mostService"></span></p>
+                            <div class="col-xs-6">
+                                <div class="cardData">
+                                    <p>Most Ordered Service</p>
+                                    <p><span id="mostService"></span></p>
+                                </div>
                             </div>
 
-                            <div class="cardData col-md-6">
-                                <p>Most Active Employee</p>
-                                <p><span id="mostActiveEmployee"></span></p>
+                            <div class="col-xs-6">
+                                <div class="cardData">
+                                    <p>Most Active Employee</p>
+                                    <p><span id="mostActiveEmployee"></span></p>
+                                </div>
                             </div>
 
-                            <div class="cardData col-md-5">
-                                <p>Overall Total Revenue</p>
-                                <p><span id="totalRevenue"></span></p>
+                            <div class="col-xs-6">
+                                <div class="cardData">
+                                    <p>Overall Total Revenue</p>
+                                    <p><span id="totalRevenue"></span></p>
+                                </div>
                             </div>
 
-                            <div class="cardData col-md-6">
-                                <p>Average Order Value</p>
-                                <p><span id="AOV"></span></p>
+                            <div class="col-xs-6">
+                                <div class="cardData">
+                                    <p>Average Order Value</p>
+                                    <p><span id="AOV"></span></p>
+                                </div>
                             </div>
 
                         </div>
