a3941a94b8000ae89c7db66054eca724b2873e6d
Artsylapp <artsylapp@gmail.com>
2024-10-09 06:19:52 +0800

    Update analytics.php

--- application/views/page/analytics.php
+++ application/views/page/analytics.php
@@ -1,6 +1,6 @@
 <div class="analytics">
     <!-- Dynamic layout with flexbox -->
-    <div class="container" style="width:90vw;">
+    <div class="container" style="width:95vw;">
         <div class="row">
 
             <div class="col-xs-9">
