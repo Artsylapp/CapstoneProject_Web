953998f9be55e25f3aaf1ba4dc0061858a333019
Artsylapp <artsylapp@gmail.com>
2024-10-15 20:21:51 +0800

    Update manage_hub.php

--- application/views/page/manage_hub.php
+++ application/views/page/manage_hub.php
@@ -10,36 +10,43 @@
 
 		</div>
 
-		<div class="row">
+		<div class="button-container">
+			<div class="container-fluid">
 
-			<div class="col-s-6 homenavbtn">
-				<a href="<?php echo $this->config->base_url("accounts")?>">
-					<button class="btn menu-btn lg-bg ttsh" name="ACCOUNTS">
-						<img class="menu-icon" src='<?php echo $this->config->base_url("assets/images/icon/icon_accounts.png");?>' alt="">
-						<h1 class="">ACCOUNTS</h1>
-					</button>
-				</a>
+			<div class="row">
+
+				<div class="col-s-6 homenavbtn">
+					<a href="<?php echo $this->config->base_url("accounts")?>">
+						<button class="btn menu-btn lg-bg ttsh" name="ACCOUNTS">
+							<img class="menu-icon" src='<?php echo $this->config->base_url("assets/images/icon/icon_accounts.png");?>' alt="">
+							<h1 class="">ACCOUNTS</h1>
+						</button>
+					</a>
+				</div>
+
+				<div class="col-s-6 homenavbtn">
+					<a href="<?php echo $this->config->base_url("services")?>">
+						<button class="btn menu-btn lbr-bg ttsh " name="SERVICES">
+							<img class="menu-icon" src='<?php echo $this->config->base_url("assets/images/icon/icon_services.png");?>' alt="">
+							<h1 class="">SERVICES</h1>
+						</button>
+					</a>
+				</div>
+
+				<div class="col-s-6 homenavbtn">
+					<a href="<?php echo $this->config->base_url("locations")?>">
+						<button class="btn menu-btn ttsh lb-bg" name="WORK STATIONS">
+							<img class="menu-icon" src='<?php echo $this->config->base_url("assets/images/icon/icon_station.png");?>' alt="">
+							<h1 class="">WORK STATIONS</h1>
+						</button>
+					</a>
+				</div>
+
+				</div>
+
 			</div>
-
-			<div class="col-s-6 homenavbtn">
-				<a href="<?php echo $this->config->base_url("services")?>">
-					<button class="btn menu-btn lbr-bg ttsh " name="SERVICES">
-						<img class="menu-icon" src='<?php echo $this->config->base_url("assets/images/icon/icon_services.png");?>' alt="">
-						<h1 class="">SERVICES</h1>
-					</button>
-				</a>
-			</div>
-
-			<div class="col-s-6 homenavbtn">
-				<a href="<?php echo $this->config->base_url("locations")?>">
-					<button class="btn menu-btn ttsh lb-bg" name="WORK STATIONS">
-						<img class="menu-icon" src='<?php echo $this->config->base_url("assets/images/icon/icon_station.png");?>' alt="">
-						<h1 class="">WORK STATIONS</h1>
-					</button>
-				</a>
-			</div>
-
 		</div>
+		
 		
 	</div>
 </div>
