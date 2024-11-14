4f630ff59636d5b4ed930c081ba7b2cd88ae24ad
senzj <151595404+senzj@users.noreply.github.com>
2024-11-14 17:46:21 +0800

    fix: bugs squashed

--- application/controllers/Records.php
+++ application/controllers/Records.php
@@ -85,9 +85,9 @@ class Records extends CI_Controller {
         // print_r($data['orders']);
 
         if (!empty($data['orders'])) {
-            echo'<pre>';
+            // echo'<pre>';
             echo json_encode($data['orders'], JSON_PRETTY_PRINT);
-            echo'</pre>';
+            // echo'</pre>';
         } else {
             echo json_encode(['error' => 'No orders found']);
         }
