7c3b5564db270d0463142988edc97028f6ce98ef
senzj <151595404+senzj@users.noreply.github.com>
2024-11-17 20:27:06 +0800

    follow up from the previous commit

--- application/views/page/locations/loc_delete.php
+++ application/views/page/locations/loc_delete.php
@@ -43,9 +43,9 @@
 						</div>
 
 						<div class="col-sm-offset-1 col-sm-6" style="display: flex;justify-content: left; padding-top: 1em;">
-							<label class="control-label col-sm-2" for="fullname">Price:</label>
+							<label class="control-label col-sm-2" for="fullname">Status:</label>
 							<div class="col-sm-10">
-								<input disabled type="text" class="form-control" id="fullname" placeholder="Fullname" value="<?php echo $price ?>">
+								<input disabled type="text" class="form-control" id="fullname" placeholder="Fullname" value="<?php echo $status ?>">
 							</div>
 						</div>
 						
