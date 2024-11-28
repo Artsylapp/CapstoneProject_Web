7c7cedc035bf7e80326c9e91111d7c196820d225
Artsylapp <artsylapp@gmail.com>
2024-11-28 21:18:14 +0800

    Update orders_info.php

--- application/views/page/orders/orders_info.php
+++ application/views/page/orders/orders_info.php
@@ -18,7 +18,51 @@
 
         <div class="row mt-s center-item">
             <div class="col-sm-12 col-xs-12 box-white">
-                
+                <form class="form-horizontal" action="<?php echo $this->config->base_url("acc_add") ?>" method="POST">
+                    
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="EmpName">Employee Name:</label>
+
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="EmpName" placeholder="Employee Name" name="create_Account" required>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="EmpAdd">Address:</label>
+
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="EmpAdd" placeholder="Address" name="create_Address" required>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="EmpCnt">Contact Number:</label>
+
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="EmpCnt" placeholder="Masseur Contact Number" name="create_Contact" required>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="sel1">Employee Type:</label>
+                        <div class="col-sm-10">
+                            <select class="form-control" id="sel1" name="optradio">
+                                <option>Masseur</option>
+                                <option>Admin</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-offset-8 col-sm-3" style="margin-top:25px;">
+                        <div class="col-sm-12">
+                            <button class="btn lg-bg menu-btn-m ttsh" name="Confirm Create Account">
+                                <h4>CREATE</h4>
+                            </button>
+                        </div>
+                    </div>
+
+                </form>
             </div>
         </div>
 
