baaf61b95de2c1eb06e2692410874210da446db5
Artsylapp <artsylapp@gmail.com>
2024-12-01 17:59:13 +0800

    Update orders_assign.php

--- application/views/page/orders/orders_assign.php
+++ application/views/page/orders/orders_assign.php
@@ -53,7 +53,7 @@
                     </thead>
                     <tbody>
                         <?php foreach($accounts as $account): ?>
-                            <?php if ($account->accounts_tbl_empType ==  $customerGender AND $account->accounts_tbl_status == "AVAILABLE"): ?>
+                            <?php if ($account->accounts_tbl_empType == $customerGender AND $account->accounts_tbl_status == "AVAILABLE"): ?>
                                 <tr>
                                     <td><?php echo $account->accounts_tbl_name; ?></td>
                                     <td><?php echo $account->accounts_tbl_empType; ?></td>
