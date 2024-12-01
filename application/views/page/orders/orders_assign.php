f8fdc422f029b9a1b8ca2b53d6d951c70911e27a
Artsylapp <artsylapp@gmail.com>
2024-12-01 17:38:50 +0800

    Update orders_assign.php

--- application/views/page/orders/orders_assign.php
+++ application/views/page/orders/orders_assign.php
@@ -1,7 +1,7 @@
 
 <script>
 
-    let customerDetailsString = localStorage.getItem('customer_details');
+    let customerDetailsString = localStorage.getItem('customer_information');
 
     if (customerDetailsString) {
         let customerDetails = JSON.parse(customerDetailsString);
@@ -44,7 +44,7 @@
                     </thead>
                     <tbody>
                         <?php foreach($accounts as $account): ?>
-                            <?php if ($account->accounts_tbl_empType ==  "<script>document.write(customerGender); console.log(customerGender)</script>" AND $account->accounts_tbl_status == "AVAILABLE"): ?>
+                            <?php if ($account->accounts_tbl_empType ==  "<script>document.write(customerGender); console.log(customerGender    )</script>" AND $account->accounts_tbl_status == "AVAILABLE"): ?>
                                 <tr>
                                     <td><?php echo $account->accounts_tbl_name; ?></td>
                                     <td><?php echo $account->accounts_tbl_empType; ?></td>
