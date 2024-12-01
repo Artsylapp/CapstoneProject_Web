fbe54943cfa379e934844ee19e18321641cf2194
Artsylapp <artsylapp@gmail.com>
2024-12-02 02:32:26 +0800

    Update homepage.php

--- application/views/page/homepage.php
+++ application/views/page/homepage.php
@@ -58,41 +58,7 @@
 			<div class="container-fluid">
 				<div class="row">
 
-					<div class="col-s-6 homenavbtn">
-						<a href="<?php echo $this->config->base_url("booking") ?>">
-							<button class="btn menu-btn lg-bg ttsh" name="BOOKING">
-								<img class="menu-icon" src='<?php echo $this->config->base_url("assets/images/icon/icon_booking.png"); ?>' alt="">
-								<h1 class="btn-text-menu btn-label">BOOKING</h1>
-							</button>
-						</a>
-					</div>
-
-					<div class="col-s-6 homenavbtn">
-						<a href="<?php echo $this->config->base_url("manage_hub") ?>">
-							<button class="btn menu-btn ttsh lb-bg" name="MANAGEMENT">
-								<img class="menu-icon" src='<?php echo $this->config->base_url("assets/images/icon/icon_manage.png"); ?>' alt="">
-								<h1 class="btn-text-menu btn-label">MANAGEMENT</h1>
-							</button>
-						</a>
-					</div>
-
-					<div class="col-s-6 homenavbtn">
-						<a href="<?php echo $this->config->base_url("records") ?>">
-							<button class="btn menu-btn lbr-bg ttsh " name="RECORDS">
-								<img class="menu-icon" src='<?php echo $this->config->base_url("assets/images/icon/icon_records.png"); ?>' alt="">
-								<h1 class="btn-text-menu btn-label">RECORDS</h1>
-							</button>
-						</a>
-					</div>
-
-					<div class="col-s-6 homenavbtn">
-						<a href="<?php echo $this->config->base_url("analytics") ?>">
-							<button class="btn menu-btn lr-bg ttsh" name="ANALYTICS">
-								<img class="menu-icon" src='<?php echo $this->config->base_url("assets/images/icon/icon_analytics.png"); ?>' alt="">
-								<h1 class="btn-text-menu btn-label">ANALYTICS</h1>
-							</button>
-						</a>
-					</div>
+					
 
 				</div>
 			</div>
